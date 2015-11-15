d83711eca6e2da48361443f4af521663b06e5e3d
Gatlin Johnson <gatlin@niltag.net>
2015-11-15 04:54:28 -0600

    removing unneeded shit

--- js/main.js
+++
@@ -1,9 +0,0 @@
-twttr.events.bind(
-  'loaded',
-  function(event) {
-      event.widgets.forEach(function(widget) {
-          console.log("Created widget: " + widget.id);
-          console.log(event);
-      });
-  }
-);
